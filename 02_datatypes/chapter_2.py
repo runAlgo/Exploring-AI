151dd177e769418ca403dedbf79c21d8ee40b086
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-02 23:15:52 +0545

    mutable and immutable started

---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,8 @@
+# mutable
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(f"Initial spice mix id: {spice_mix}")
+spice_mix.add("Ginger")
+spice_mix.add("Cardamom")
+print(f"After spice mix id: {spice_mix}")
+print(f"After spice mix id: {id(spice_mix)}")
