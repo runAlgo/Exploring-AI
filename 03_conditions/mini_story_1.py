0098eca728e6f7814db0bba3477b712086f2972b
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-16 15:04:51 +0545

    Implement notification system for smart kettle with boiling check

---
+++ 03_conditions/mini_story_1.py
@@ -0,0 +1,10 @@
+# You're creating a notification system for a smart kettle.
+# It should remind the user only when the kettle has finished
+# boiling. Task:
+# -- A variable kettle_boiled = True
+# -- If boiled, show: "Kettle done! Time to make chai!"
+
+kettle_boiled = False
+
+if kettle_boiled:
+    print("Kettle done! Time to make chai!")
