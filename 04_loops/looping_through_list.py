bdd3f9beaeee00c4514e469e6847dec66761fbdf
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-19 14:26:40 +0545

    Add initial implementation for printing chai order queue

---
+++ 04_loops/looping_through_list.py
@@ -0,0 +1,9 @@
+# You receive a list of names for chai orders.
+# The goal is to print out the order queue.
+# Task: 
+# Use a list of names.
+# Print: "Order ready for [name]"
+
+names = ["Alice", "Bob", "Charlie", "David", "Eva"]
+for name in names:
+    print(f"Order ready for {name}")
