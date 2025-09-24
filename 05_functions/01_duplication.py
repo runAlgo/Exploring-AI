acfe79c34a1eeb3895c3fdbbba39e9682ca4fdb1
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-09-24 20:21:09 +0545

    Add initial implementation of order printing function for chai stall

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,16 @@
+# You're managing a busy tea stall.
+#  you receive orders and want to print each customer's name
+#  along with the type of chai they ordered.
+# Task:-
+# Write a function print_order(name, chai_type)
+# Call it multiple times with different customers
+
+def print_order(name, chai_type):
+    print(f"Customer: {name}, Chai Type: {chai_type}!")
+
+
+
+
+print_order("Alice", "Masala Chai")
+print_order("Dipak", "Ginger Chai")
+print_order("Rabi", "Lemon Chai")
